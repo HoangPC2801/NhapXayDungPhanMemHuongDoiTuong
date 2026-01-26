57d0f147ff8aee9df432d61f882a0f6b001ba041
HoangPC2801 <hoangtucuncon5s1@gmail.com>
2026-01-26 17:53:12 +0700

    thay code moi

---
+++ BizFlow.ProductAPI/DbModels/Product.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BizFlow.ProductAPI.DbModels
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        [Required, MaxLength(200)]
+        public string Name { get; set; } = string.Empty; // VD: Xi măng Hà Tiên
+
+        [Required, MaxLength(50)]
+        public string Sku { get; set; } = string.Empty; // VD: XM_HT_01
+
+        public string BaseUnit { get; set; } = string.Empty; // VD: Kg (Đơn vị gốc để tính kho)
+
+        public string? ImageUrl { get; set; }
+        public string? Description { get; set; }
+        
+        public bool IsActive { get; set; } = true; // 👈 Mới thêm
+
+        // --- LIÊN KẾT ---
+        public Inventory Inventory { get; set; } 
+        public ICollection<ProductUnit> ProductUnits { get; set; } = new List<ProductUnit>();
+    }
+}
--- src/Clients/bizflow_mobile/android/app/src/main/kotlin/com/example/bizflow_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.bizflow_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
