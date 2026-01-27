e8c799568bfe3f65311c613f6a9339709f3d6cc0
HoangPC2801 <hoangtucuncon5s1@gmail.com>
2026-01-27 09:42:40 +0700

    thay code

--- BizFlow.ProductAPI/DTOs/CreateCategoryRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace BizFlow.ProductAPI.DTOs
-{
-    public class CreateCategoryRequest
-    {
-        // Thêm = string.Empty để tránh lỗi null
-        public string Name { get; set; } = string.Empty;
-        public string Code { get; set; } = string.Empty;
-    }
-}
